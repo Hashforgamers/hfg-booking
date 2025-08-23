f068f40a1352b7fa9604bd9216f0f616069cdec0
bhanujoshi <jbhanu244@gmail.com>
2025-08-23 14:51:51 +0200

    Added is_pay_at_cafe

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -154,6 +154,7 @@ def create_booking():
     slot_ids = data.get("slot_id")  # list expected
     game_id = data.get("game_id")
     book_date = data.get("book_date")
+    is_pay_at_cafe = data.get("is_pay_at_cafe")
 
     log.info("bookings.post.payload cid=%s slot_ids_len=%s game_id=%s book_date=%s",
              cid, (len(slot_ids) if isinstance(slot_ids, list) else None), game_id, book_date)
