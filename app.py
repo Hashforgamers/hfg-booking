b0f9cdf63b38f2965b1a67ffeddf59bbccd3eff2
bhanujoshi <jbhanu244@gmail.com>
2025-09-27 18:33:02 +0200

    Fixed booking

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from app import create_app
 
-app, socketio = create_app()  # expose both to Gunicorn
+# Create the Flask app
+app, socketio = create_app()
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5054, debug=True)
